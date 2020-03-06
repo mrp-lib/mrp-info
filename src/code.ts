6a8a01990f12a7f2d41b5760b54dc49b014c256c
Lu Yizhi <yizhi@Lus-Mac-Pro.local>
2020-03-06 22:24:31 +0800

    对\u0000进行过滤

--- src/code.ts
+++ src/code.ts
@@ -5,5 +5,6 @@ var Iconv = require('iconv').Iconv
  * @param src 要转换的数据
  */
 export function gbk2utf8(src: Buffer | string, coding?: string): Buffer {
-	return new Iconv(coding || 'gbk', 'utf8').convert(src)
+	const buf: Buffer = new Iconv(coding || 'gbk', 'utf8').convert(src)
+	return Buffer.from(buf.filter(s => s !== 0))
 }
